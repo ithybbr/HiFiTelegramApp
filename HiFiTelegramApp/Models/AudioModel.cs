b7afb83a96fea2d2514fdc2c41d0f224efa3e57a
s.zhakenov <sherniyazz@gmail.com>
2025-07-14 09:48:59 +0500

    The idea is to have fields with file name and file path, and onclick it will create an audio player which will manage audio. I don't yet understand how it will work, but I want it to be part of layout with controls.

---
+++ HiFiTelegramApp/Models/AudioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HiFiTelegramApp.Models;
+
+public class AudioModel
+{
+    public int SongId { get; init; }
+    public string Path { get; init; } = string.Empty;
+    public string Artist { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+}
