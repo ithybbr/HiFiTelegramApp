702768ab06c98396531dce2f3d7adfdda8ff4cbe
s.zhakenov <sherniyazz@gmail.com>
2025-07-15 11:15:34 +0500

    do aurora js

--- HiFiTelegramApp/wwwroot/js/site.js
+++ HiFiTelegramApp/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿loadAjax = (url) =>{
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(data){
+            $('#container').html(data);
+        }
+    })
+}
