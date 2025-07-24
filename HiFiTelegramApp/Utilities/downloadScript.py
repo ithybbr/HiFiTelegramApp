a0ef06149e82e952078acf48dbef17e56022e08e
Sherniyaz Zhakenov <zhakenov.sherni@gmail.com>
2025-07-24 23:26:51 +0500

    prolly fixed out of loop dummy 1 error

--- HiFiTelegramApp/Utilities/downloadScript.py
+++ HiFiTelegramApp/Utilities/downloadScript.py
@@ -13,16 +13,13 @@ def start_up(message_id: int) -> str:
     bot_token = os.getenv("BOT_TOKEN")
     channel_id = os.getenv("CHANNEL_ID")  # e.g. "-1001234567890"
 
-    # 3) run the client synchronously
-    with Client(
+    return Client(
         "hifimusic_bot",
         api_id=api_id,
         api_hash=api_hash,
         bot_token=bot_token
-    ) as app:
-        # if you want to download a message from a channel:
-        # add the chat_id: f"{channel_id}/{message_id}"
-        message = app.get_messages("hifimusicfromtidal", message_id)
-        path = app.download_media(message, file_name = "wwwroot/downloads/")
-        return path
-    
+    ).run(download_song, channel_id, message_id)
+
+async def download_song(app: Client, channel_id: str, message_id: int):
+    file = await app.download_media(f"{channel_id}/{message_id}")
+    return file.name
